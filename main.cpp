a4a357385edf79cc0d0a87df71adab223cb1b668
Oscar Baracos <oscar.baracos@gmail.com>
2020-10-01 22:45:31 -0400

    Add writing to file, deletion, and seeing insertions

--- main.cpp
+++ main.cpp
@@ -14,13 +14,22 @@ list<unique_ptr<list<char>>> input;
 WINDOW *win;
 int x, y, max_x, max_y;
 
-void traverse_list() {
+void traverse_list(ostream &out = cout) {
   for (auto const &line : input) {
     for (char c : *line) {
-      cout << c;
+      out << c;
     }
-    cout << endl;
+    out << endl;
   }
+}
+
+void write_output(char *filename) {
+  ofstream out;
+  out.open(filename);
+
+  traverse_list(out);
+
+  out.close();
 }
 
 void init_tui() {
@@ -35,7 +44,27 @@ void init_tui() {
   wrefresh(win);
 }
 
+void print_text() {
+  erase();
+  move(0, 0);
+  refresh();
+
+  auto it = input.begin();
+  for (; it != input.end(); ++it) {
+    for (auto line_it = (*it)->begin(); line_it != (*it)->end(); ++line_it) {
+      addch(*line_it);
+    }
+
+    if (*it != input.back()) {
+      addch('\n');
+    }
+  }
+
+  refresh();
+}
+
 void do_tui() {
+  // TODO: merge with print_text
   auto it = input.begin();
   for (; it != input.end(); ++it) {
     for (auto line_it = (*it)->begin(); line_it != (*it)->end(); ++line_it) {
@@ -61,70 +90,78 @@ void do_tui() {
   std::locale loc;
   wchar_t ch;
   while ((ch = getch()) != CTRL_X) {
-    switch (ch) {
-      case KEY_LEFT:
-        if (x > 0) {
-          --x;
+    // Using if-else's to catch printable characters
+    if (ch == KEY_LEFT) {
+      if (x > 0) {
+        --x;
+        --line_it;
+      }
+    } else if (ch == KEY_RIGHT) {
+      // TODO: Don't assume lines don't overflow
+      if (x < (int) (*it)->size() - 1 && x < max_x) {
+        ++x;
+        ++line_it;
+      }
+    } else if (ch == KEY_UP) {
+      if (y > 0) {
+        --y;
+        --it;
+        if (x == 0) {
+          line_it = (*it)->begin();
+        } else if (x < (int) (*it)->size()) {
+          // x within line size, don't change it
+          line_it = (*it)->begin();
+          std::advance(line_it, x - 1);
+        } else {
+          line_it = (*it)->end();
           --line_it;
-        }
-        break;
-      case KEY_RIGHT:
-        // TODO: Don't assume lines don't overflow
-        if (x < (int) (*it)->size() - 1 && x < max_x) {
-          ++x;
-          ++line_it;
-        }
-        break;
-      case KEY_UP:
-        if (y > 0) {
-          --y;
-          --it;
-          if (x == 0) {
-            line_it = (*it)->begin();
-          } else if (x < (int) (*it)->size()) {
-            // x within line size, don't change it
-            line_it = (*it)->begin();
-            std::advance(line_it, x - 1);
+          if ((*it)->empty()) {
+            x = 0;
           } else {
-            line_it = (*it)->end();
-            --line_it;
-            if ((*it)->empty()) {
-              x = 0;
-            } else {
-              x = (int) (*it)->size() - 1;
-            }
+            x = (int) (*it)->size() - 1;
           }
         }
-        break;
-      case KEY_DOWN:
-        if (y < (int) input.size() - 1 && y < max_y) {
-          ++y;
-          ++it;
-          if (x == 0) {
-            line_it = (*it)->begin();
-          } else if (x < (int) (*it)->size()) {
-            // x within line size, don't change it
-            line_it = (*it)->begin();
-            std::advance(line_it, x - 1);
+      }
+    } else if (ch == KEY_DOWN) {
+      if (y < (int) input.size() - 1 && y < max_y) {
+        ++y;
+        ++it;
+        if (x == 0) {
+          line_it = (*it)->begin();
+        } else if (x < (int) (*it)->size()) {
+          // x within line size, don't change it
+          line_it = (*it)->begin();
+          std::advance(line_it, x - 1);
+        } else {
+          line_it = (*it)->end();
+          --line_it;
+          if ((*it)->empty()) {
+            x = 0;
           } else {
-            line_it = (*it)->end();
-            --line_it;
-            if ((*it)->empty()) {
-              x = 0;
-            } else {
-              x = (int) (*it)->size() - 1;
-            }
+            x = (int) (*it)->size() - 1;
           }
         }
-        break;
-      default:
-        // Check if alpha numeric character was pressed
-        if (!std::isalpha(ch, loc)) {
-          break;
-        }
-        
-        (*it)->insert(line_it, ch);
-        break;
+      }
+    } else if (ch == KEY_BACKSPACE) {
+      // TODO: handle deleting line
+      if (x == 0) continue;
+
+      line_it = (*it)->erase(line_it);
+      // erase returns the next iterator, go to previous instead
+      --line_it;
+      --x;
+      print_text();
+    } else if (std::isprint(ch, loc)) {
+      // TODO: let lines overflow
+      if ((int) (*it)->size() == max_x) continue;
+
+      (*it)->insert(line_it, ch);
+      ++x;
+      // auto temp_it = input.begin();
+      print_text();
+    } else {
+      // TODO: handle newline
+      continue;
     }
 
     wmove(win, y, x);
@@ -135,8 +172,8 @@ void do_tui() {
 }
 
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    cout << "Usage: " << argv[0] << " <filename>" << endl;
+  if (!(argc == 2 || argc == 3)) {
+    cout << "Usage: " << argv[0] << " <filename> <output>" << endl;
     exit(1);
   }
 
@@ -167,7 +204,11 @@ int main(int argc, char **argv) {
 
   do_tui();
 
-  traverse_list();
+  if (argc == 3) {
+    write_output(argv[2]);
+  } else {
+    traverse_list();
+  }
 
   return 0;
 }
